79940a294f6c42799e57f872950bfb3269dde05e
Busid <desarrolloweb214@gmail.com>
2025-05-08 17:27:39 +0200

    change package.json

---
+++ client/dist/e-commerce/browser/chunk-CEFFUISZ.js
@@ -0,0 +1,1 @@
+import"./chunk-TRNF7YA4.js";var o=[{path:"login",loadComponent:()=>import("./chunk-UGC6VDWV.js")},{path:"register",loadComponent:()=>import("./chunk-K4DZATLW.js")}];export{o as default};
---
+++ client/dist/e-commerce/browser/chunk-NP6KQIII.js
@@ -0,0 +1,1 @@
+import"./chunk-TRNF7YA4.js";var o=[{path:"",loadComponent:()=>import("./chunk-O5ZMMLZM.js")},{path:"checkout",loadComponent:()=>import("./chunk-74B4BFJD.js")}];export{o as default};
